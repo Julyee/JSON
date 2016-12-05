f8a45cd386dce0f8f57c05d2f56b56c7734ffc36
Dario Segura <daio.seco@gmail.com>
2016-12-05 00:06:52 -0500

    [PARSER] Initial parser implementation.

---
+++ JSON/Parser.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Text;
+
+namespace Julyee.JSON
+{
+    public class Parser
+    {
+        public enum Operationtype
+        {
+            ObjecStart,
+            ObjectEnd,
+            ArrayStart,
+            ArrayEnd,
+            Key,
+            StringValue,
+            LiteralValue,
+            AppendSibling
+        }
+
+        public delegate void ParseHandler(Operationtype operation, string content, int depth);
+
+        internal static void Parse(string jsonString, ParseHandler handler)
+        {
+            Reader reader = new Reader(jsonString);
+            StringBuilder builder = new StringBuilder();
+
+            char? startCharacter = reader.GetNextControlCharacter();
+
+            if (startCharacter == '{')
+            {
+                ParseObject(reader, builder, handler, 0);
+            }
+            else if (startCharacter == '[')
+            {
+                ParseArray(reader, builder, handler, 0);
+            }
+            else
+            {
+                throw new Exception("Julyee.Parser the provided string is malformed JSON.");
+            }
+        }
+
+        internal static void ParseObject(Reader reader, StringBuilder builder, ParseHandler handler, int depth)
+        {
+            handler(Operationtype.ObjecStart, "{", depth);
+
+            char controlChar = reader.GetNextControlCharacterSafe();
+
+            while (controlChar != '}')
+            {
+                if (controlChar != '\"')
+                {
+                    throw new Exception("Malformed JSON. Object keys must be enclosed by quotes (\").");
+                }
+
+                string key = reader.ReadString(builder);
+                handler(Operationtype.Key, key, depth);
+
+                controlChar = reader.GetNextControlCharacterSafe();
+                if (controlChar != ':')
+                {
+                    throw new Exception("Malformed JSON. Expected \":\" after \"" + key + "\".");
+                }
+                builder.Length = 0;
+
+                controlChar = reader.GetNextControlCharacterSafe();
+                if (Reader.IsControlCharacter(controlChar))
+                {
+                    if (controlChar == '{')
+                    {
+                        ParseObject(reader, builder, handler, depth + 1);
+                    }
+                    else if (controlChar == '[')
+                    {
+                        ParseArray(reader, builder, handler, depth + 1);
+                    }
+                    else if (controlChar == '\"')
+                    {
+                        string value = reader.ReadString(builder);
+                        handler(Operationtype.StringValue, value, depth);
+                    }
+                }
+                else
+                {
+                    builder.Append(controlChar);
+                    string value = reader.ReadValue(builder);
+                    handler(Operationtype.LiteralValue, value, depth);
+                }
+
+                builder.Length = 0;
+                controlChar = reader.GetNextControlCharacterSafe();
+
+                if (controlChar == ',')
+                {
+                    controlChar = reader.GetNextControlCharacterSafe();
+                    handler(Operationtype.AppendSibling, ",", depth);
+                }
+                else if (controlChar != '}')
+                {
+                    throw new Exception("Malformed JSON. Expected delimeter in object.");
+                }
+            }
+
+            handler(Operationtype.ObjectEnd, "}", depth);
+        }
+
+        internal static void ParseArray(Reader reader, StringBuilder builder, ParseHandler handler, int depth)
+        {
+            handler(Operationtype.ArrayStart, "[", depth);
+
+            char controlChar = reader.GetNextControlCharacterSafe();
+
+            while (controlChar != ']')
+            {
+                if (Reader.IsControlCharacter(controlChar))
+                {
+                    if (controlChar == '{')
+                    {
+                        ParseObject(reader, builder, handler, depth + 1);
+                    }
+                    else if (controlChar == '[')
+                    {
+                        ParseArray(reader, builder, handler, depth + 1);
+                    }
+                    else if (controlChar == '\"')
+                    {
+                        string value = reader.ReadString(builder);
+                        handler(Operationtype.StringValue, value, depth);
+                    }
+                }
+                else
+                {
+                    builder.Append(controlChar);
+                    string value = reader.ReadValue(builder);
+                    handler(Operationtype.LiteralValue, value, depth);
+                }
+
+                builder.Length = 0;
+                controlChar = reader.GetNextControlCharacterSafe();
+
+                if (controlChar == ',')
+                {
+                    controlChar = reader.GetNextControlCharacterSafe();
+                    handler(Operationtype.AppendSibling, ",", depth);
+                }
+                else if (controlChar != ']')
+                {
+                    throw new Exception("Malformed JSON. Expected delimeter in array.");
+                }
+            }
+
+            handler(Operationtype.ArrayEnd, "]", depth);
+        }
+    }
+}
---
+++ JSON/Reader.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Julyee.JSON
+{
+    public class Reader : StringReader
+    {
+        public Reader(string jsonString) : base(jsonString)
+        {
+            // default constructor
+        }
+
+        public string ReadString(StringBuilder builder = null)
+        {
+            if (builder == null)
+            {
+                builder = new StringBuilder();
+            }
+
+            for (char ch = GetNextCharacterSafe(); ch != '\"'; ch = GetNextCharacterSafe())
+            {
+                if (ch == '\\')
+                {
+                    builder.Append(ch);
+                    ch = GetNextCharacterSafe();
+                    builder.Append(ch);
+                }
+                else
+                {
+                    builder.Append(ch);
+                }
+            }
+            return builder.ToString();
+        }
+
+        public string ReadValue(StringBuilder builder = null)
+        {
+            if (builder == null)
+            {
+                builder = new StringBuilder();
+            }
+
+            for (char ch = PeekNextCharacterSafe(); !IsControlCharacter(ch) && !char.IsWhiteSpace(ch); ch = PeekNextCharacterSafe())
+            {
+                Read();
+                builder.Append(ch);
+            }
+            return builder.ToString();
+        }
+
+        public char GetNextControlCharacterSafe(bool returnOnOther = true)
+        {
+            char? ch = GetNextControlCharacter(returnOnOther);
+            if (ch == null)
+            {
+                throw new Exception("Malformed JSON file. Unexpected EOF");
+            }
+
+            return (char)ch;
+        }
+
+        public char? GetNextControlCharacter(bool returnOnNonBlank = true)
+        {
+            bool skip = false;
+            for (char? ch = GetNextCharacter(); ch != null; ch = GetNextCharacter())
+            {
+                if (skip)
+                {
+                    skip = false;
+                    continue;
+                }
+
+                if (IsControlCharacter(ch) || (returnOnNonBlank && !char.IsWhiteSpace((char)ch)))
+                {
+                    return ch;
+                }
+
+                if (ch == '\\')
+                {
+                    skip = true;
+                }
+            }
+            return null;
+        }
+
+        public char GetNextCharacterSafe()
+        {
+            char? ch = GetNextCharacter();
+            if (ch == null)
+            {
+                throw new Exception("Malformed JSON file. Unexpected EOF");
+            }
+            return (char) ch;
+        }
+
+        public char? GetNextCharacter()
+        {
+            int ch = Read();
+            if (ch == -1)
+            {
+                return null;
+            }
+            return Convert.ToChar(ch);
+        }
+
+        public char PeekNextCharacterSafe()
+        {
+            char? ch = PeekNextCharacter();
+            if (ch == null)
+            {
+                throw new Exception("Malformed JSON file. Unexpected EOF");
+            }
+            return (char) ch;
+        }
+
+        public char? PeekNextCharacter()
+        {
+            int ch = Peek();
+            if (ch == -1)
+            {
+                return null;
+            }
+            return Convert.ToChar(ch);
+        }
+
+        public static bool IsControlCharacter(char? ch)
+        {
+            return ch == '{' || ch == '}' ||
+                   ch == '[' || ch == ']' ||
+                   ch == ':' || ch == ',' ||
+                   ch == '\"';
+        }
+    }
+}
